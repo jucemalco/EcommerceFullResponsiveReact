2ea891c97ce2b3dff51b0429f7125215eadce435
JulioM <jucemalco@gmail.com>
2022-06-09 10:32:54 -0400

    Product creado, card

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Product from './components/Product';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Esto es el directorio</h1>
+      <Product/>
+    </div>
+  );
+}
+
+export default App;
